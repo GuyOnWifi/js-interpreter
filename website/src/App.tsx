e761ee7a871704fa2ccab5b493582d61b216c71f
Eason Huang <easonh887@gmail.com>
2025-01-03 20:40:54 -0500

    Website to showcase the interpreter

---
+++ website/src/App.tsx
@@ -0,0 +1,98 @@
+import { useState } from 'react'
+import CodeMirror from '@uiw/react-codemirror';
+import { vscodeDark } from '@uiw/codemirror-theme-vscode';
+import { javascript } from '@codemirror/lang-javascript';
+import { EditorView, EditorState } from '@uiw/react-codemirror';
+
+// @ts-ignore
+import Lexer from "./interpreter/lexer.js";
+// @ts-ignore
+import Parser from "./interpreter/parser.js";
+// @ts-ignore
+import Executor from "./interpreter/executor.js";
+
+import './App.css'
+
+function App() {
+  const [code, setCode] = useState(`print "Hello World!";`);
+  const [output, setOutput] = useState(`Output will show up here...`);
+
+  function runCode() {
+    setOutput("");
+    // Lexing
+    const lexer = new Lexer(code);
+    
+    let tokens = [];
+    tokens.push(lexer.next());
+
+    while (tokens[tokens.length - 1].type !== "EOF") {
+        tokens.push(lexer.next());
+    }
+
+    // Parsing
+    const parser = new Parser(tokens);
+    let ast;
+    try {
+      ast = parser.createTree();
+    } catch(err : any) {
+      return setOutput("Error: " + err.message);
+    }
+
+    console.dir(ast, {depth: null});
+
+    // Executing
+    const executor = new Executor(ast, setOutput);
+    try {
+      executor.execute();
+    } catch(err : any) {
+      return setOutput("Error: " + err.message);
+    }
+  }
+
+  return (
+    <>
+      <div className="flex flex-row h-[100vh] w-[100vw] max-h-[100vh] max-w-[100vw] gap-4 p-4">
+
+        <div className="flex-1 flex flex-col max-w-[50%]">
+          <div className="h-10 flex flex-row items-center">
+            <span className="bg-[#1e1e1e] self-end p-2 font-bold">Code</span>
+            <span className="ml-auto bg-green-500 px-4 py-2 rounded hover:bg-green-400 cursor-pointer" onClick={runCode}>Run</span>
+          </div>
+          <CodeMirror
+            id="code"
+            value={code}
+            theme={[vscodeDark, javascript()]}
+            basicSetup={{
+              autocompletion: false,
+            }}
+            extensions={[EditorView.lineWrapping]}
+            className="grow overflow-scroll"
+            onChange={setCode}
+          />
+        </div>
+
+        <div className="flex-1 flex flex-col max-w-[50%]">
+          <div className="h-10 flex flex-row items-center">
+            <span className="bg-[#1e1e1e] self-end p-2 font-bold">Output</span>
+          </div>
+          <CodeMirror
+            id="console"
+            value={output}
+            theme={[vscodeDark]}
+            basicSetup={{
+              lineNumbers: false,
+              highlightActiveLine: false,
+              highlightActiveLineGutter: false
+            }}
+            extensions={[EditorView.editable.of(false), EditorState.readOnly.of(true), EditorView.lineWrapping]}
+            className="grow overflow-scroll"
+          />
+        </div>
+
+      </div>
+    </>
+
+  )
+}
+
+export default App;
---
+++ website/src/interpreter/executor.js
@@ -0,0 +1,163 @@
+export default class Executor {
+    constructor(ast, setOutput) {
+        this.ast = ast;
+        this.setOutput = setOutput;
+        this.variables = [];
+        this.functions = [];
+    }
+
+    searchIdentifiers(id) {
+        // Loop backwards (like a stack)
+        for (let i = this.variables.length - 1; i >= 0; i--) {
+            if (this.variables[i].identifier === id) {
+                return this.variables[i];
+            }
+        }
+        return null;
+    }
+
+    execNode(node, inFunctionCall = false) {
+        if (node === undefined) return;
+        if (node.type === "VariableDeclaration") {
+            if (this.searchIdentifiers(node.identifier)) {
+                throw new Error(`Duplicate identifier ${node.identifier}`);
+            }
+            this.variables.push({
+                identifier: node.identifier,
+                kind: node.kind,
+                value: node.value
+            });
+            return;
+        } else if (node.type === "FunctionDeclaration") {
+            this.functions.push({
+                identifier: node.identifier,
+                body: node.body,
+                params: node.params
+            })
+            return;
+        } else if (node.type === "IfStatement") {
+            if (this.execNode(node.test)) {
+                this.execNode(node.consequent);
+            } else {
+                this.execNode(node.alternate);
+            }
+            return;
+        } else if (node.type === "WhileStatement") {
+            while (this.execNode(node.test)) {
+                this.execNode(node.body);
+            }
+            return;
+        } else if (node.type === "BlockStatement") {
+            for (const n of node.body) {
+                const val = this.execNode(n, inFunctionCall);
+                if (val && val.type === "return") {
+                    return val.value;
+                }
+            }
+            return;
+        } else if (node.type === "CallExpression") {
+            const func = this.functions.find(f => f.identifier === node.callee);
+            if (!func) {
+                throw new Error(`Undefined function ${node.callee}`);
+            }
+
+            // push the arguments into variables
+            const basePointer = this.variables.length;
+            for (let i = 0; i < func.params.length; i++) {
+                this.variables.push({
+                    identifier: func.params[i],
+                    value: node.arguments[i],
+                    kind: "let"
+                })
+            }
+
+            const res = this.execNode(func.body, true);
+
+            // remove the arguments
+            this.variables.splice(basePointer, this.variables.length - 1);
+
+            return res;
+        } else if (node.type === "ReturnStatement") {
+            if (!inFunctionCall) {
+                throw new Error("Return outside of function call");
+            }
+            return {
+                type: "return",
+                value: this.execNode(node.argument)
+            };
+        } else if (node.type === "Print") {
+            const val = this.execNode(node.value);
+            this.setOutput((output) => {
+                return output + val + "\n";
+            });
+            return val;
+        } else if (node.type === "Identifier") {
+            const id = this.searchIdentifiers(node.name);
+            if (!id) {
+                throw new Error(`Identifier "${node.name}" does not exist`);
+            }
+
+            return id.value;
+        } else if(node.type === "AssignmentExpression") {
+            const id = this.searchIdentifiers(node.left);
+            if (!id) {
+                throw new Error(`Identifier ${node.left} does not exist`);
+            }
+            if (id.kind === "const") {
+                throw new Error(`Assignment on constant variable "${id.identifier}"`)
+            }
+            id.value = this.execNode(node.right);
+        } else if (node.type === "UnaryExpression") {
+            if (node.operator === "+") {
+                return this.execNode(node.argument);
+            } else if (node.operator === "-") {
+                return -this.execNode(node.argument);
+            } else if (node.operator === "!") {
+                return !this.execNode(node.argument);
+            } 
+        } else if (node.type === "BinaryExpression") {
+            if (node.operator === "+") {
+                return this.execNode(node.left) + this.execNode(node.right);
+            } else if (node.operator === "-") {
+                return this.execNode(node.left) - this.execNode(node.right);
+            } else if (node.operator === "*") {
+                return this.execNode(node.left) * this.execNode(node.right);
+            } else if (node.operator === "/") {
+                return this.execNode(node.left) / this.execNode(node.right);
+            }
+        } else if (node.type === "ComparisonExpression") {
+            if (node.operator === ">") {
+                return this.execNode(node.left) > this.execNode(node.right);
+            } else if (node.operator === ">=") {
+                return this.execNode(node.left) >= this.execNode(node.right);
+            } else if (node.operator === "<") {
+                return this.execNode(node.left) < this.execNode(node.right);
+            } else if (node.operator === "<=") {
+                return this.execNode(node.left) <= this.execNode(node.right);
+            } else if (node.operator === "==") {
+                return this.execNode(node.left) === this.execNode(node.right);
+            } else if (node.operator === "!=") {
+                return this.execNode(node.left) !== this.execNode(node.right);
+            }
+        } else if (node.type === "LogicalExpression") {
+            if (node.operator === "&&") {
+                return this.execNode(node.left) && this.execNode(node.right);
+            } else if (node.operator === "||") {
+                return this.execNode(node.left) || this.execNode(node.right);
+            } 
+        }
+        
+        return node;
+        
+    }
+
+    execute() {
+        if (this.ast.type !== "Program") {
+            throw new Error("Invalid ast");
+        }
+
+        for (const n of this.ast.body) {
+            this.execNode(n);
+        }
+    }
+}
